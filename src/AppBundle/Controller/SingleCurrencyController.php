54390b41e8210e6bbec7da53289ac6f45f84e1f6
Felikss Kamisevs <felix115x@icloud.com>
2020-04-06 11:35:25 +0300

    Finishing up

--- src/AppBundle/Controller/SingleCurrencyController.php
+++ src/AppBundle/Controller/SingleCurrencyController.php
@@ -4,28 +4,31 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Service\CurrencyService;
 
-class MonitoringPlotController extends Controller
+class SingleCurrencyController extends Controller
 {
+    const MAX_DAYS = 10; // TODO could be passed as parameter
+
     /**
-     * @Route("/monitoring", name="monitoring")
+     * @Route("/currency{code}", name="currency")
      */
-    public function indexAction(Request $request)
+    public function indexAction($code)
     {
         $currencyService = $this->container->get('currency_service');
-        $response = $currencyService->getExchangeData(null, 5);
+        $response = $currencyService->getExchangeData(null, self::MAX_DAYS);
 
         $rates = $response;
-        $recentDates = $currencyService->getRecentDates(5);
+        $recentDates = $currencyService->getRecentDates(self::MAX_DAYS);
         $currencies = $currencyService->getCurrencyData();
 
-        return $this->render('monitoring/plots.html.twig', [
+        return $this->render('currency/currency.html.twig', [
             'exchange_rates' => $rates,
             'recent_dates' => $recentDates,
-            'currencies' => array_column($currencies, 'code'),
+            'code' => $code,
         ]);
     }
 }
